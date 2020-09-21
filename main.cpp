2ae4ad726ad2962214ae89cef46bffe9f002fa45
kbuffardi <kbuffardi@csuchico.edu>
2020-09-21 13:05:40 -0700

    Add buggy code

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	bool age;
+
+	cout<<"Hi, what's your name?";
+	getline(cin,name);
+
+	cout<<name<<,<<what is your age?;
+	cin>>age>>endl;
+
+	if( age >0 && age < 12 )
+		cout<<"Hey kid!"<<endl;
+	if( age < 18 )
+		cout<<"I hope you're enjoying highschool!"<<endl;
+	else
+		cout<<"Oh, you're an adult. Nice to meet you."<<endl;
+
+	return 0;
+}
